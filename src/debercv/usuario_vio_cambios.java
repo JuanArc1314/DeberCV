c4c2d09889d3ddf512416654750ac8cbab679d6e
moral <moral@HPAnahi>
2026-05-07 23:51:02 -0500

    Se agrego la clase usuario_vio_cambios

---
+++ src/debercv/usuario_vio_cambios.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package debercv;
+
+/**
+ *
+ * @author moral
+ */
+public class usuario_vio_cambios {
+    
+    /*Método : Desde el menú principal (el más rápido)
+        
+    Abre el archivo que te interesa en el editor de NetBeans.
+
+    Ve al menú superior y selecciona Team (Equipo).
+
+    En el menú desplegable, haz clic en Show Annotations (Mostrar anotaciones)*/
+    
+}
