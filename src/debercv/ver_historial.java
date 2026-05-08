33e3f18e049446276729206f0f451a7b91a14a6d
User <User@SEGUNDO>
2026-05-08 10:16:01 -0500

    Se creo la clase ver_historial

---
+++ src/debercv/ver_historial.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package debercv;
+
+/**
+ *
+ * @author User
+ */
+public class ver_historial {
+/*Ver historial Git en NetBeans
+Abre tu proyecto en NetBeans.
+En el menú superior entra a:
+Team → Show History
+Se abrirá una ventana con:
+los commits realizados,
+fecha,
+autor,
+mensajes/comentarios.*/
+}
