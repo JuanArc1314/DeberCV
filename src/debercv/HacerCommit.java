7ec56def4d303619e4dcf9623647d7fa89d786fd
THThoms <msolis5357@uta.edu.ec>
2026-05-08 10:06:00 -0500

    Se realizo el contenido de como hacer un commit

---
+++ src/debercv/HacerCommit.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package debercv;
+
+/**
+ *
+ * @author USER
+ */
+public class HacerCommit {
+    /*
+ * Un commit es una fotografía del estado actual del proyecto en un momento
+ * determinado. Cada vez que se realizan cambios en el código y se desea
+ * guardarlos en el historial de Git, se ejecuta un commit para registrar
+ * qué archivos cambiaron, qué se modificó y quién lo hizo.
+ *
+ * En NetBeans: clic derecho en el proyecto → Git → Commit
+ * Se seleccionan los archivos modificados, se escribe un mensaje descriptivo
+ * y se confirma. Los cambios quedan guardados localmente.
+ *
+ * Para compartirlos con el equipo se debe ejecutar un Push, que envía
+ * los commits al repositorio remoto en GitHub.
+ */
+}
